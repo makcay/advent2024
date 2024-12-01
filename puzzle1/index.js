677b73947a25b917525856a748d3aaf654614135
makcay <makcay1982@gmail.com>
2024-12-01 09:08:49 +0000

    fileOperations.js

--- puzzle1/index.js
+++ puzzle1/index.js
@@ -1,19 +1,6 @@
-import path from 'node:path';
-import fs from 'fs';
-import readline from 'readline';
+import fo from './fileOperations.js'
 
-const __dirname = import.meta.dirname;
-
-function getFileStream(filename) {
-    const fileStream = fs.createReadStream(path.join(__dirname, filename));
-    return readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-    });    
-}
-
-
-for await (const line of getFileStream('test1.txt')) {
-    // Each line in input.txt will be successively available here as `line`.
-    console.log(`Line from file: ${line}`);
+let data = await fo.readFile('test1.txt');
+for (const line of data) {
+    console.log(`line=${line}`)
 }
