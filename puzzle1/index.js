e2e0c558e0a5a70188c2c3f7fd9a629a89a6b1af
makcay <makcay1982@gmail.com>
2024-12-02 17:05:00 +0000

    puzzle2

--- puzzle1/index.js
+++ puzzle1/index.js
@@ -1,6 +1,24 @@
 import fo from './fileOperations.js'
 
 let data = await fo.readFile('test1.txt');
+let list1=[], list2=[]
 for (const line of data) {
-    console.log(`line=${line}`)
+    let matches = line.match(/\d+/g);
+    list1.push(matches[0])
+    list2.push(matches[1])
 }
+
+function distance() {
+    list1.sort()
+    return list2.sort()
+        .map((e,i) =>  Math.abs(e-list1[i]))
+        .reduce((sum, diff) => sum+diff);
+}
+
+function similarity() {
+    return list1.map( e1 =>
+        e1 * list2.filter(e2=>e1==e2).length
+    ).reduce((sum, diff) => sum+diff);
+}
+
+console.log(similarity())
---
+++ puzzle3/index.js
@@ -0,0 +1,23 @@
+import fo from './fileOperations.js'
+
+let data = await fo.readFile('test.txt');
+let reports = []
+for (const line of data) {
+    let numbers = line.match(/\d+/g).map(a=>Number(a));
+    reports.push(numbers)
+}
+
+function isReportSafe(report) {
+    if (report.length<2) return true;
+    if (report[0]==report[1]) return false;
+    let isIncreasing=report[1]>report[0];
+    for(let i=1;i<report.length;i++) {
+        if (Math.abs(report[i]-report[i-1])>3) return false;
+        if (report[i]==report[i-1]) return false;
+        if (isIncreasing!==report[i]>report[i-1]) return false;
+    }
+    return true;
+}
+
+console.log(reports.map(r=>isReportSafe(r)).filter(r=>r==true).length);
+
