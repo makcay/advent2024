131e15dc230c4449b62f1f83747b19779c5042de
makcay <makcay1982@gmail.com>
2024-12-03 13:09:19 +0000

    puzzle3

--- puzzle3/index.js
+++ puzzle3/index.js
@@ -1,23 +1,40 @@
 import fo from './fileOperations.js'
 
+let total = 0
 let data = await fo.readFile('test.txt');
-let reports = []
+let isDoMode = true;
 for (const line of data) {
-    let numbers = line.match(/\d+/g).map(a=>Number(a));
-    reports.push(numbers)
-}
-
-function isReportSafe(report) {
-    if (report.length<2) return true;
-    if (report[0]==report[1]) return false;
-    let isIncreasing=report[1]>report[0];
-    for(let i=1;i<report.length;i++) {
-        if (Math.abs(report[i]-report[i-1])>3) return false;
-        if (report[i]==report[i-1]) return false;
-        if (isIncreasing!==report[i]>report[i-1]) return false;
+    let modifiedLine;
+    if (isDoMode) {
+        modifiedLine = "do()" + line
+    } else {
+        modifiedLine = "don't()" + line
     }
-    return true;
+
+    let filteredLine = removeDonts(modifiedLine)
+    let multiplications = filteredLine.match(/mul\(([1-9]\d{0,2}|0),([1-9]\d{0,2}|0)\)/g);
+    multiplications.forEach(m=>{
+        let numbers = m.match((/\d+/g)).map(a=>Number(a))
+        total += numbers[0]*numbers[1]
+    });
 }
 
-console.log(reports.map(r=>isReportSafe(r)).filter(r=>r==true).length);
+function removeDonts(line) {
+    let start = line.indexOf('don\'t()');
+    if (start != -1) {
+        isDoMode = false
+        let result = line.substr(0, start);
+        let remaining = line.substr(start)
+        let end = remaining.indexOf('do()')
+        if (end != -1) {
+            let tmp = remaining.substr(end+4)
+            isDoMode = tmp.indexOf("don't") == -1
+            result = result + tmp
+        }
+        return removeDonts(result);
+    }
+    return line;
+}
+
+console.log(total);
 
